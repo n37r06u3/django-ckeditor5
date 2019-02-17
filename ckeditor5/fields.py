0ef21a8056b88cda8c4c6ee7fce0a1e1cd1e9917
N37R06U3 <n37r06u3@gmail.com>
2019-02-17 21:20:32 +0800

    Update fields.py

--- ckeditor5/fields.py
+++ ckeditor5/fields.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-# @Author: panc25
-# @Date:   2018-09-05 16:07:04
-# @Last Modified by:   panc25
-# @Last Modified time: 2018-09-05 21:20:10
-
 from __future__ import absolute_import
 
 from django import forms
@@ -15,7 +9,7 @@ from .widgets import CKEditorWidget
 class RichTextField(models.TextField):
 
     def __init__(self, *args, **kwargs):
-        self.config_name = kwargs.pop('config_name', 'default')
+        self.config_name = kwargs.pop("config_name", "default")
         super(RichTextField, self).__init__(*args, **kwargs)
 
     def formfield(self, **kwargs):
@@ -28,13 +22,11 @@ class RichTextField(models.TextField):
 
     @staticmethod
     def _get_form_class():
-        return RichTextField
+        return RichTextFormField
 
 
 class RichTextFormField(forms.fields.CharField):
 
-    def __init__(self, config_name='default', *args, **kwargs):
-        kwargs.update({
-            'widget': CKEditorWidget(config_name=config_name)
-        })
+    def __init__(self, config_name='default',  *args, **kwargs):
+        kwargs.update({'widget': CKEditorWidget(config_name=config_name)})
         super(RichTextFormField, self).__init__(*args, **kwargs)
